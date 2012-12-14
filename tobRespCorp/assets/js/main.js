c87efc8cf3e80587fb73dc2d55b7f00103100f69
Tobias Lund <tobias.p.lund@gmail.com>
2012-12-14 16:24:41 +0100

    New resp coorp

---
+++ tobRespCorp/assets/js/main.js
@@ -0,0 +1,8 @@
+﻿$(function () { 
+    
+    $(".desktop").removeClass("desktop").addClass('_desktop');
+    $(".tablet").removeClass("tablet").addClass('_tablet');
+    $(".phone").removeClass("phone").addClass('_phone');
+    $(".info").removeClass("info").addClass('_info');
+    
+});
--- tobSlider/assets/js/main.js
+++ tobSlider/assets/js/main.js
@@ -1,161 +1,183 @@
 ﻿var APP = {};
 
 $(function () {
-
-    $('.tobSlider').tobSlider();
-
-    APP.container = $('.tobSlider');
-    APP.sliderWidth = APP.container.width();
-    APP.sliderHeight = APP.container.height();
-    APP.sliceCount = 6;
-    APP.sliceWidthFloat = APP.sliderWidth / APP.sliceCount;
-    APP.sliceWidthInt = parseInt(APP.sliceWidthFloat);
-    APP.lastSliceWidthAdd = APP.sliderWidth - APP.sliceWidthInt * APP.sliceCount;
-    APP.sliderObjects = [];
-    
-    $.each($('.tobSlider img'), function (index, img) {
-        var i = new sliderObject(img, index);
-        APP.sliderObjects.push(i);        
-    });
-
-    setInterval(function () {
-        $.each(APP.sliderObjects, function (index, obj) {
-            if (obj.index == 0) {
-                obj.index = APP.sliderObjects.length;
-                obj.doEffect(function () {
-                    $.each(APP.sliderObjects, function (index, _obj) {
-                        _obj.index--;
-                    });
-                });
-                return false;
-            }
-        });
-    }, 4000);
+    $('.test').tobSlider();
 });
 
+(function ($) {
 
-sliderObject = function (domElement, index) {
-    if (!$(domElement).is('img'))
-        return;
+    $.fn.tobSlider = function (options) {
+        var self = $(this);
+        var self2 = this;
+        var i = 1;
+        return this.each(function (index, slider) {
+            var sliderWrapper = $(this);
+            var _tobSlider = new TobSlider(sliderWrapper, options);
+        });
+    };
+
+    //Default settings
+    $.fn.tobSlider.defaultSettings = {
+        effect: 'tobSliderSlideUp',
+        cols: 8,
+        rows: 4,
+        speed: 500,
+        interval: 3000
+    };
+
+})(jQuery);
+
+var TobSlider = function (wrapper, options) {
     
-    this._image = domElement;
-    this._effect = $(domElement).attr('data-tobSlider-effect');
-    this._index = index;
-    switch (index) {
-        case 0:
-            prepareForEffect(this._effect, this._image);
-            $(this._image).remove();
-            break;
-        case 1:
-            APP.container.append(this._image);
-            break;
-        default:
-            $(this._image).remove();
+    // Merge argument settings with default
+    var settings = $.extend({}, $.fn.tobSlider.defaultSettings, options);
+
+    var variables = {
+        sliceCount: 0,
+        sliderWidth: 0,
+        sliderHeight: 0,
+        currentSlice: 0,
+        onGoing: false
+    };
+
+    // Get slider and set class tobSlider if it is not already have it
+    var slider = $(wrapper);
+    if (!slider.hasClass('tobSlider'))
+        slider.addClass('tobSlider');
+
+    // Get slider dimensions
+    variables.sliderWidth = slider.width();
+    variables.sliderHeight = slider.height();
+
+    // Enumerate slider childrens add get imageCollection
+    var childrens = slider.children();
+    var imgCollection = [];
+    childrens.each(function () {
+        var child = $(this);
+        if (child.is('img')) {
+            var tobSliderImage = new TobSliderImage(child, variables, settings);
+            imgCollection.push(tobSliderImage);
+        }
+        else
+            child.remove(); // TODO: Shouldn't remove. Add functionality for other selectors
+        variables.sliceCount++;
+    });
+
+    if(variables.sliceCount > 1){
+        slider.append(imgCollection[0].slices).append(imgCollection[1].slices);
+        imgCollection[0].prepare();
+        imgCollection[1].upNext();
+    }
+
+    var timer = setInterval(function () { doSlider(variables, imgCollection, null) }, settings.interval);
+    
+    var doSlider = function (variables, imgCollection, callback) {
+
+        variables.onGoing = true;
+        imgCollection[variables.currentSlice].doEffect(function () {
+            
+            variables.currentSlice = variables.currentSlice + 1 > variables.sliceCount - 1 ? 0 : variables.currentSlice + 1;
+            var sliceUpNext = variables.currentSlice + 1 > variables.sliceCount - 1 ? 0 : variables.currentSlice + 1;
+            var slicesUpNext = imgCollection[sliceUpNext].slices;
+
+            imgCollection[variables.currentSlice].prepare();
+            slider.append(slicesUpNext);
+            imgCollection[sliceUpNext].upNext();
+            if(callback)
+                callback.apply(null);
+            variables.onGoing = false;
+        });        
+    }
+
+    slider.click(function () {
+        if (!variables.onGoing) {
+            clearInterval(timer);
+            doSlider(variables, imgCollection, function () {
+                timer = setInterval(function () { doSlider(variables, imgCollection, null) }, settings.interval);
+            });
+        }
+    });
+
+
+}
+
+var TobSliderImage = function (img, sliderVariables, settings) {
+    var image = $(img);
+    var rows = image.attr('data-tobSlider-rows') === undefined ? settings.rows : parseInt(image.attr('data-tobSlider-rows'));
+    var cols = image.attr('data-tobSlider-cols') === undefined ? settings.cols : parseInt(image.attr('data-tobSlider-cols'));
+    var sliceHeight = parseInt(sliderVariables.sliderHeight / rows);
+    var lastSliceHeight = sliderVariables.sliderHeight - sliceHeight * rows + sliceHeight;
+    var sliceWidth = parseInt(sliderVariables.sliderWidth / cols);
+    var lastSliceWidth = sliderVariables.sliderWidth - sliceWidth * cols + sliceWidth;
+    var slice;
+    this._slices = [];
+    this._effect = image.attr('data-tobSlider-effect') === '' ? settings.effect : image.attr('data-tobSlider-effect');
+
+    image.remove();
+
+    for (var row = 0; row < rows; row++) {
+        for (var col = 0; col < cols; col++) {
+            if (row == rows - 1) {
+                if (col == cols - 1)
+                    slice = $('<div class="slice" style="width: ' + lastSliceWidth + 'px; height: ' + lastSliceHeight + 'px; left: ' + col * sliceWidth + 'px; top: ' + row * sliceHeight + 'px"><img src="' + image.attr('src') + '" style="left: -' + col * sliceWidth + 'px; top: -' + row * sliceHeight + 'px"/></div>');
+                else
+                    slice = $('<div class="slice" style="width: ' + sliceWidth + 'px; height: ' + lastSliceHeight + 'px; left: ' + col * sliceWidth + 'px; top: ' + row * sliceHeight + 'px"><img src="' + image.attr('src') + '" style="left: -' + col * sliceWidth + 'px; top: -' + row * sliceHeight + 'px"/></div>');
+            }
+            else {
+                if (col == cols - 1)
+                    slice = $('<div class="slice" style="width: ' + lastSliceWidth + 'px; height: ' + sliceHeight + 'px; left: ' + col * sliceWidth + 'px; top: ' + row * sliceHeight + 'px"><img src="' + image.attr('src') + '" style="left: -' + col * sliceWidth + 'px; top: -' + row * sliceHeight + 'px"/></div>');
+                else
+                    slice = $('<div class="slice" style="width: ' + sliceWidth + 'px; height: ' + sliceHeight + 'px; left: ' + col * sliceWidth + 'px; top: ' + row * sliceHeight + 'px"><img src="' + image.attr('src') + '" style="left: -' + col * sliceWidth + 'px; top: -' + row * sliceHeight + 'px"/></div>');
+            }
+            this._slices.push(slice);
+        }
     }
 
     var self = this;
+
     return {
-        prepareForEffect: function () { prepareForEffect(self._effect, self._image) },
-        doEffect: function (callback) { doEffect(self._effect, callback) },
-        get index() {
-            return self._index;
+        slices: self._slices,
+        upNext: function () {
+            $.each(self._slices, function (index, slice) {
+                slice.css('display', 'block').css('z-index', '0');
+            });
         },
-        get effect() { return this._effect },
-        set index(value) {
-            var oldValue = self._index;
-            self._index = value;
-            switch (value) {
-                case 0:
-                    $(self._image).remove();
-                    this.prepareForEffect();
-                    break;
-                case 1:
-                    APP.container.append(self._image);
-                    break;
-                default:
+        prepare: function () {
+            $.each(self._slices, function (index, slice) {
+                slice.css('display', 'block').css('z-index', '998');
+            });
+        },
+        doEffect: function (callback) {
+            var _callback = null;
+            for (var i = 0; i < self._slices.length; i++) {
+                if (i == self._slices.length - 1)
+                    _callback = callback;
+                (function (slice, timeOut) {
+                    if (_callback) {
+                        setTimeout(function () {
+                            window[self._effect](slice, function () {
+                                slice.remove();
+                                _callback.apply(null);
+                            });
+                        }, timeOut);
+                    }
+                    else {
+                        setTimeout(function () {
+                            window[self._effect](slice, function () {
+                                slice.remove();
+                            });
+                        }, timeOut);
+                    }
+                })(self._slices[i], 100 + i*50, _callback);
             }
         }
     }
 }
 
-function prepareForEffect(effect, image) {
-    switch (effect) {
-        case 'mySlideUp':
-            prepareSlideUpEffect(image);
-            break;
-        case 'myFadeOut':
-            prepareSlideUpEffect(image);
-            break;
-        default:
-    }
+function tobSliderSlideUp(element, callback) {
+    element.slideUp(100, null, callback);
 }
 
-function prepareSlideUpEffect(img) {
-    var slice;
-    var image;
-    var offsetLeft = 0;
-    for (var i = 0; i < APP.sliceCount; i++) {
-
-        slice = $('<div class="slice"></div>');
-        slice.css('height', APP.sliderHeight).css('left', offsetLeft).css('z-index', 999);
-
-        if (i == APP.sliceCount - 1)
-            slice.css('width', APP.sliceWidthInt + APP.lastSliceWidthAdd);
-        else
-            slice.css('width', APP.sliceWidthInt);
-
-        image = new Image();
-        image.src = img.src;
-
-        slice.append(image);
-        slice.children('img').css('left', -1 * offsetLeft);
-
-        APP.container.append(slice);
-        offsetLeft += APP.sliceWidthInt;
-    }
-}
-
-function doEffect(effect, callback) {
-    var slices = $('.tobSlider .slice');
-
-    for (var i = 0; i < slices.length; i++) {
-
-        if(i == 0){
-            timeout(slices[i], 1, null, effect);
-        }            
-        else if(i == slices.length-1)
-            timeout(slices[i], 100 + i * 100, callback, effect);
-        else
-            timeout(slices[i], 100 + i * 100, null, effect);
-    }
-}
-
-function mySlideUp(element, speed, callback) {
-    $(element).slideUp(speed, null, callback);
-}
-
-function myFadeOut(element, speed, callback) {
-    $(element).fadeOut(speed, null, callback);
-}
-
-function timeout(element, delay, callback, effect) {
-    setTimeout(function () {
-
-        window[effect](element, 600, function () {
-            $(element).remove();
-            if (callback)
-                callback.apply(this, null);
-        });
-    }, delay);
-}
-
-(function ($) {
-    $.fn.tobSlider = function (options) {
-        var self = $(this);
-
-    };
-})(jQuery);
-
-var TobSlider = function (wrapper, options) {
-    
+function tobSliderFadeOut(element, callback) {
+    element.fadeOut(100, null, callback);
 }
